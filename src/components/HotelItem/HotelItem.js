b66615d486849704d58ca2b4e9826cd9f3f7b156
d0ge3 <fnik340@gmail.com>
2021-03-20 00:39:34 +0300

    feat: add icon in HotelItem

--- src/components/HotelItem/HotelItem.js
+++ src/components/HotelItem/HotelItem.js
@@ -4,23 +4,27 @@ import { FavouriteIcon } from '../FavouriteIcon/FavouriteIcon'
 import { StarsIndicator } from '../StarsIndicator/StarsIndicator'
 
 import s from './HotelItem.module.scss'
+import { HouseIcon } from '../HouseIcon/HouseIcon'
 
-export const HotelItem = ({ style }) => {
+export const HotelItem = ({ style, showIcon }) => {
   return (
     <div style={style} className={s.wrapper}>
-      <div className={s.titleWrap}>
-        <span className={s.title}>Moscow Mariott Grand Hotel</span>
-        <FavouriteIcon />
-      </div>
-      <div className={s.dateWrap}>
-        <span className={s.date}>28 June 2020</span>
-        <span className={s.days}>1 день</span>
-      </div>
-      <div className={s.levelWrap}>
-        <StarsIndicator countStars={1} />
-        <div className={s.priceWrap}>
-          <span className={s.price}>Price:</span>
-          <span className={s.priceValue}>23 924Р</span>
+      {showIcon && <HouseIcon style={{ marginRight: '24px' }} />}
+      <div className={s.infoWrapper}>
+        <div className={s.titleWrap}>
+          <span className={s.title}>Moscow Mariott Grand Hotel</span>
+          <FavouriteIcon />
+        </div>
+        <div className={s.dateWrap}>
+          <span className={s.date}>28 June 2020</span>
+          <span className={s.days}>1 день</span>
+        </div>
+        <div className={s.levelWrap}>
+          <StarsIndicator countStars={1} />
+          <div className={s.priceWrap}>
+            <span className={s.price}>Price:</span>
+            <span className={s.priceValue}>23 924Р</span>
+          </div>
         </div>
       </div>
     </div>
--- src/components/HotelsWrapper/HotelsWrapper.js
+++ src/components/HotelsWrapper/HotelsWrapper.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import s from './HotelsWrapper.module.scss'
+import { HotelItem } from '../HotelItem/HotelItem'
 
 export const HotelsWrapper = () => {
   return (
     <div className={s.wrapper}>
-
+      <HotelItem showIcon />
     </div>
   )
 }
