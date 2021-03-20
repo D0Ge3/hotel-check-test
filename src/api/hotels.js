43cc98f0e64e7ca9f794ac94c1ab38d6d99119f8
d0ge3 <fnik340@gmail.com>
2021-03-20 22:08:18 +0300

    use https api

--- src/api/hotels.js
+++ src/api/hotels.js
@@ -4,7 +4,7 @@ export const hotelsApi = {
   getHotels(checkIn, checkOut, location) {
     return axios
       .get(
-        `http://engine.hotellook.com/api/v2/cache.json?location=${location}&currency=rub&checkIn=${checkIn}&checkOut=${checkOut}&limit=100`
+        `https://engine.hotellook.com/api/v2/cache.json?location=${location}&currency=rub&checkIn=${checkIn}&checkOut=${checkOut}&limit=100`
       )
       .then((res) => res)
   },
