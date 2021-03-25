d6709792d3721bff59e9673a34241691856a70fc
d0ge3 <fnik340@gmail.com>
2021-03-25 13:33:01 +0300

    fix: add redirect from / to /login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Login } from './pages/Login/Login'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import { Hotels } from './pages/Hotels/Hotels'
 
 export const App = () => {
   require('moment/locale/ru.js')
   return (
     <Router>
+      <Redirect from="/" to="/login" />
       <Route path="/login">
         <Login />
       </Route>
